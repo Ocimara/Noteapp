8116cca2b81f58e23b6441d7b49bdf34a606cbe6
logonrm <logonrm@fiap.com.br>
2018-03-03 11:02:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ocimara/noteapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ocimara.noteapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_cadastro, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.getItemId()) {
+            R.id.nmPesquisar -> {
+
+                return true
+            }
+            R.id.nmSalvar -> {
+
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
