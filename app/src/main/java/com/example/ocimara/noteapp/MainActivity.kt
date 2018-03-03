b19a1c43f6aa688177a76decb797954a3a86d7c4
ocimara <ocimarafbarcellos@com.br>
2018-03-03 15:11:53 -0300

    sabado dia 3/3

--- app/src/main/java/com/example/ocimara/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/ocimara/noteapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
